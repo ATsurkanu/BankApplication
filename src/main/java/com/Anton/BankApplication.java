44ebbdbbe2566f395d887c580cf29f0aa1ffc89b
ATsurkanu <AMTsurkanu@gmail.com>
2018-12-11 19:45:54 +0200

    Commented code was removed

--- src/main/java/com/Anton/BankApplication.java
+++ src/main/java/com/Anton/BankApplication.java
@@ -27,30 +27,4 @@ public class BankApplication  {
 		ApplicationContext ctx = SpringApplication.run(BankApplication.class, args);
 
 	}
-
-	/*@Override
-	public void run(String... arg0) throws Exception {
-
-		Customer cust1 = customerRepository.save(new Customer("Anton", "Anton@gmail.com"));
-		Customer cust2 = customerRepository.save(new Customer("asma", "asma@gmail.com"));
-
-		Account currAcc = accountRepository.save(new CurrentAccount("currAcc", new Date(), 9000, cust1, 6000));
-		Account savAcc = accountRepository.save(new SavingsAccount("savAcc", new Date(), 6000, cust2, 5.5));
-
-		Operation paymentOpcurrAcc = operationRepository.save(new PaymentOperation(new Date(), 2000, currAcc));
-		Operation widhdrawelOpcurrAcc = operationRepository.save(new WithdrawalOperation(new Date(), 1000, currAcc));
-		Operation paymentOp1currAcc = operationRepository.save(new PaymentOperation(new Date(), 2000, currAcc));
-		Operation widhdrawelOp1currAcc = operationRepository.save(new WithdrawalOperation(new Date(), 1000, currAcc));
-		Operation paymentOp2currAcc = operationRepository.save(new PaymentOperation(new Date(), 5000, currAcc));
-		Operation widhdrawelOp2currAcc = operationRepository.save(new WithdrawalOperation(new Date(), 1000, currAcc));
-		Operation paymentOp3currAcc = operationRepository.save(new PaymentOperation(new Date(), 8741, currAcc));
-		Operation widhdrawelOp3currAcc = operationRepository.save(new WithdrawalOperation(new Date(), 2000, currAcc));
-		Operation paymentOp4currAcc= operationRepository.save(new PaymentOperation(new Date(), 6587, currAcc));
-		Operation widhdrawelOp4currAcc = operationRepository.save(new WithdrawalOperation(new Date(), 2547, currAcc));
-
-		Operation paymentOpsavAcc = operationRepository.save(new PaymentOperation(new Date(), 1000, savAcc));
-		Operation widhdrawelOpsavAcc = operationRepository.save(new WithdrawalOperation(new Date(), 2000, savAcc));
-
-		iBankService.payToAccount(currAcc.getAccountId(), 99999999);
-	} */
 }
--- src/main/java/com/Anton/controller/BankController.java
+++ src/main/java/com/Anton/controller/BankController.java
@@ -26,6 +26,48 @@ public class BankController {
         return "accounts";
     }
 
+    @RequestMapping("/getAccount")
+    public String getAccountById(@RequestParam(name = "accountId") String accountId, Model model,
+                                 @RequestParam(name = "page", defaultValue = "0") int page) {
+        model.addAttribute("accountIdModel", accountId);
+        try {
+            Account account = bankService.getAccountById(accountId);
+            model.addAttribute("accountModel", account);
+            Page<Operation> pageOperation = bankService.getAccountOperationByPage(accountId, page, 4);
+            int pageNumber = pageOperation.getTotalPages();
+            Integer[] pages = new Integer[pageNumber];
+            for (int i = 0; i < pageNumber; i++) {
+                pages[i] = i;
+            }
+            model.addAttribute("pageOperationsModel", pageOperation);
+            model.addAttribute("pagesModel", pages);
+
+        } catch (Exception e) {
+            model.addAttribute("exceptionModel", e);
+        }
+
+        return "accounts";
+    }
+
+    @RequestMapping(value = "/saveAccountOperation", method = RequestMethod.POST)
+    public String saveAccountOperation(Model model, String accountId, String operationType, String accountIdDest,
+                                       @RequestParam(defaultValue = "0") double operationAmount) {
+        try {
+            if (operationType.equals("PAYMENT")) {
+                bankService.payToAccount(accountId, operationAmount);
+            } else if (operationType.equals("WITHDRAWAL")) {
+                bankService.removeFromAccount(accountId, operationAmount);
+            } else {
+                bankService.transfer(accountId, accountIdDest, operationAmount);
+            }
+        } catch (Exception e) {
+            model.addAttribute("errorModel", e);
+            return "redirect:/getAccount?accountId=" + accountId + "&errorModel=" + e.getMessage();
+        }
+
+        return "redirect:/getAccount?accountId=" + accountId;
+    }
+
     @RequestMapping(value = "/createAccount", method = RequestMethod.POST)
     public String createNewAccountForExistingCustomer(@RequestParam(name = "customerName") String customerName,
                                                       @RequestParam(name = "customerEmail") String customerEmail,
@@ -67,48 +109,6 @@ public class BankController {
         }
 
         return "accounts";
-    }
-
-    @RequestMapping("/getAccount")
-    public String getAccountById(@RequestParam(name = "accountId") String accountId, Model model,
-                                 @RequestParam(name = "page", defaultValue = "0") int page) {
-        model.addAttribute("accountIdModel", accountId);
-        try {
-            Account account = bankService.getAccountById(accountId);
-            model.addAttribute("accountModel", account);
-            Page<Operation> pageOperation = bankService.getAccountOperationByPage(accountId, page, 4);
-            int pageNumber = pageOperation.getTotalPages();
-            Integer[] pages = new Integer[pageNumber];
-            for (int i = 0; i < pageNumber; i++) {
-                pages[i] = i;
-            }
-            model.addAttribute("pageOperationsModel", pageOperation);
-            model.addAttribute("pagesModel", pages);
-
-        } catch (Exception e) {
-            model.addAttribute("exceptionModel", e);
-        }
-
-        return "accounts";
-    }
-
-    @RequestMapping(value = "/saveAccountOperation", method = RequestMethod.POST)
-    public String saveAccountOperation(Model model, String accountId, String operationType, String accountIdDest,
-                                       @RequestParam(defaultValue = "0") double operationAmount) {
-        try {
-            if (operationType.equals("PAYMENT")) {
-                bankService.payToAccount(accountId, operationAmount);
-            } else if (operationType.equals("WITHDRAWAL")) {
-                bankService.removeFromAccount(accountId, operationAmount);
-            } else {
-                bankService.transfer(accountId, accountIdDest, operationAmount);
-            }
-        } catch (Exception e) {
-            model.addAttribute("errorModel", e);
-            return "redirect:/getAccount?accountId=" + accountId + "&errorModel=" + e.getMessage();
-        }
-
-        return "redirect:/getAccount?accountId=" + accountId;
     }
 
 }
